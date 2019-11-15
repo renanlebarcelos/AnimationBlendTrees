5cca7eeb120814626e1fe612117819e53cada794
Renan Barcelos <renan.barcelos@outlook.com>
2019-11-15 12:06:55 -0300

    First commit

---
+++ AnimationBlendTrees/Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            _anim.SetBool("Hook", true);
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            _anim.SetBool("Jab", true);
+        }
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            _anim.SetBool("CrossJab", true);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AnimationBlendTrees
 Trabalho com animação utilizando Animator da Unity.
+
+# Informações sobre uso
+Teclas:
+
+As teclas para aplicar os golpes são:
+
+H - Hook (Gancho)
+J - Jab & Cross (Jab e Cruzado)
+K - Jad & Cross Body (Jab e Cruzado no corpo)
+
+Ao apertar duas ou mais dessas teclas, as animações rodam em sequência.
+
+Configuração:
+Animação criada no Makehuman, com rig feito e animações feitos no Mixamo. Texturas do próprio Makehuman. Animação com animator da Unity, utilizando blend trees.
